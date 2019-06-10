f6d89854749bfe48fade56c676fb710d20286133
VieuxChameau <VieuxChameau@users.noreply.github.com>
2019-06-09 21:53:00 -0700

    Add more unit tests

--- src/test/kotlin/org/vieuxchameau/SelectionSortKtTest.kt
+++ src/test/kotlin/org/vieuxchameau/SelectionSortKtTest.kt
@@ -1,6 +1,7 @@
 package org.vieuxchameau
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -8,17 +9,47 @@ class SelectionSortKtTest {
     @CsvSource(
             "0",
             "1",
+            "54",
+            "79",
             "100"
     )
     @ParameterizedTest
     fun `should sort array`(size: UInt) {
         val numbers = arrayOfRandomInts(size)
 
-        println("Array = ${numbers.asString()}")
-        selectionSort(numbers)
+        val sortedNumbers = selectionSort(numbers)
 
-        assertThat(numbers).isSorted()
-        println("Sorted Array = ${numbers.asString()}")
+        assertThat(sortedNumbers).isSorted()
+    }
+
+
+    @Test
+    fun `should sort already ASC ordered array`() {
+        val numbers = IntArray(15) { it * 2 }
+
+        val sortedNumbers = selectionSort(numbers)
+
+        assertThat(sortedNumbers).isSorted()
+    }
+
+    @Test
+    fun `should sort already DESC ordered array`() {
+        val numbers = IntArray(15) { it * 2 }
+
+        numbers.reverse()
+
+        val sortedNumbers = selectionSort(numbers)
+
+        assertThat(sortedNumbers).isSorted()
+    }
+
+    @Test
+    fun `should sort array of similar values`() {
+        val numbers = IntArray(2) { 42 }
+
+        val sortedNumbers = selectionSort(numbers)
+
+        assertThat(sortedNumbers).isSorted()
     }
 }
 
